b1410c5369b58f43b083fab26ce542750e69a207
oleksandraburymenko <115561772+oleksandraburymenko@users.noreply.github.com>
2023-02-24 19:51:26 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,9 +1,8 @@
 
 const fontSizeControl = document.querySelector('input#font-size-control');
 const span = document.querySelector('span#text');
-
+span.style.fontSize = fontSizeControl.value + 'px';
 fontSizeControl.addEventListener('input', onInputChange);
 function onInputChange (event) {
-  const fontSize = event.target.value + 'px';
-  span.style.fontSize = fontSize;
+  span.style.fontSize = event.target.value + 'px';
 };
