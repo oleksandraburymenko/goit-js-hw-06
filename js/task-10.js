f168707168f6fe8c2976ad07687fe5af0b7dc01d
oleksandraburymenko <115561772+oleksandraburymenko@users.noreply.github.com>
2023-02-24 21:21:38 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,34 @@
+const input = document.querySelector('input');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const boxesContainer = document.querySelector('div#boxes');
+
+
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
+function createBoxes () {
+  const amount = Number(input.value);
+
+  const boxes = [];
+  let boxSize = 30;
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement("div");
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+    boxSize += 10;
+  }
+  boxesContainer.append(...boxes);
+
+  
+}
+
+function destroyBoxes () {
+  boxesContainer.innerHTML = '';
+
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
